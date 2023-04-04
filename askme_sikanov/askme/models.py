80bde6018320d30b5348417d4d3be8befad6c3d3
Maxim Sikanov <max.4251474@yandex.ru>
2023-04-04 18:20:03 +0300

    templates

--- askme_sikanov/askme/models.py
+++ askme_sikanov/askme/models.py
@@ -1,6 +1,15 @@
 QUESTIONS = [
     {
+        'id' : i,
         'title' : f'Question {i}',
+        'text' : f'Text {i}',
+    } for i in range(3)
+]
+
+
+ANSWERS = [
+    {
+        'id' : i,
         'text' : f'Text {i}',
     } for i in range(3)
 ]
--- askme_sikanov/askme/views.py
+++ askme_sikanov/askme/views.py
@@ -7,5 +7,27 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def question(request):
-    return render(request, 'question.html')
+def question(request, question_id):
+    context = {'question': models.QUESTIONS[question_id], 'answers' : models.ANSWERS}
+    return render(request, 'question.html', context)
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def tag(request):
+    context = {'questions': models.QUESTIONS}
+    return render(request, 'tag.html', context)
