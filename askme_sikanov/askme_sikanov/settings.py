522aa8df7c4c105e7981adab94041710dee98a66
Max425 <max.4251474@yandex.ru>
2023-05-24 07:06:57 +0300

    fix

--- askme_sikanov/askme_sikanov/settings.py
+++ askme_sikanov/askme_sikanov/settings.py
@@ -132,11 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-
-MEDIA_ROOT = BASE_DIR / 'media/' 
-MEDIA_URL = '/media/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -145,3 +143,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 APPEND_SLASH = True
 LOGIN_URL = '/login'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/' 
